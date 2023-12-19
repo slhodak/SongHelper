84ebaa04bb33b3cfc284051d9b29880f9ae45704
Sam Hodak <slhodak@gmail.com>
2023-12-19 15:01:09 -0300

    Add adsr

--- iOS/SongHelper/ContentView.swift
+++ iOS/SongHelper/ContentView.swift
@@ -11,37 +11,38 @@ import SoundpipeAudioKit
 
 
 struct ContentView: View {
-    private var cMajChordPlayer = ChordPlayer([60, 64, 67])
-    private var dMinChordPlayer = ChordPlayer([62, 65, 69])
-    private var eMinChordPlayer = ChordPlayer([64, 67, 71])
-    private var fMajChordPlayer = ChordPlayer([60, 65, 69])
-    private var gMajChordPlayer = ChordPlayer([62, 67, 71])
-    private var aMinChordPlayer = ChordPlayer([60, 64, 69])
-    private var bDimChordPlayer = ChordPlayer([62, 65, 71])
+    private var polyphonicPlayer = PolyphonicPlayer(voices: 3)
     
     var body: some View {
         VStack {
-            ChordButton(title: "CMaj") {
-                cMajChordPlayer.playFor(duration: 1)
-            }
-            ChordButton(title: "Dmin") {
-                dMinChordPlayer.playFor(duration: 1)
-            }
-            ChordButton(title: "Emin") {
-                eMinChordPlayer.playFor(duration: 1)
-            }
-            ChordButton(title: "FMaj") {
-                fMajChordPlayer.playFor(duration: 1)
-            }
-            ChordButton(title:"GMaj") {
-                gMajChordPlayer.playFor(duration: 1)
-            }
-            ChordButton(title: "Amin") {
-                aMinChordPlayer.playFor(duration: 1)
-            }
-            ChordButton(title: "Bdim") {
-                bDimChordPlayer.playFor(duration: 1)
-            }
+            ChordButton(
+                title: "CMaj",
+                onPress: { polyphonicPlayer.noteOn(notes: [60, 64, 67], for: 1) },
+                onUnpress: polyphonicPlayer.noteOff)
+            ChordButton(
+                title: "Dmin",
+                onPress: { polyphonicPlayer.noteOn(notes: [62, 65, 69], for: 1) },
+                onUnpress: polyphonicPlayer.noteOff)
+            ChordButton(
+                title: "Emin",
+                onPress: { polyphonicPlayer.noteOn(notes: [64, 67, 71], for: 1) },
+                onUnpress: polyphonicPlayer.noteOff)
+            ChordButton(
+                title: "FMaj",
+                onPress: { polyphonicPlayer.noteOn(notes: [60, 65, 69], for: 1) },
+                onUnpress: polyphonicPlayer.noteOff)
+            ChordButton(
+                title: "GMaj",
+                onPress: { polyphonicPlayer.noteOn(notes: [62, 67, 71], for: 1) },
+                onUnpress: polyphonicPlayer.noteOff)
+            ChordButton(
+                title: "Amin",
+                onPress: { polyphonicPlayer.noteOn(notes: [60, 64, 69], for: 1) },
+                onUnpress: polyphonicPlayer.noteOff)
+            ChordButton(
+                title: "Bdim",
+                onPress: { polyphonicPlayer.noteOn(notes: [62, 65, 71], for: 1) },
+                onUnpress: polyphonicPlayer.noteOff)
         }
     }
 }
@@ -50,32 +51,25 @@ struct ContentView: View {
     ContentView()
 }
 
-class ChordPlayer {
+class PolyphonicPlayer {
     let engine = AudioEngine()
     var oscillators: [Oscillator] = []
     let mixer = Mixer()
-    let adsr = ADSREnvelope()
+    var envelopes: Array<AmplitudeEnvelope> = []
     
-    init(_ chordNotes: [Int]) {
-        // Configure ADSR envelope
-        adsr.attackDuration = 0.1  // Adjust as needed
-        adsr.decayDuration = 0.1   // Adjust as needed
-        adsr.sustainLevel = 0.8    // Adjust as needed
-        adsr.releaseDuration = 0.1 // Adjust as needed
-        
-        for note in chordNotes {
+    init(voices: Int) {
+        for _ in 0...voices {
             let osc = Oscillator()
-            osc.frequency = AUValue(note).midiNoteToFrequency()
-            let oscWithEnv = osc => adsr
-            oscillators.append(oscWithEnv)
-        }
-        
-        for osc in oscillators {
             osc.amplitude = 0.2
-            mixer.addInput(osc)
+            osc.start()
+            oscillators.append(osc)
+            
+            let envelope = createEnvelope(osc: osc)
+            envelopes.append(envelope)
+            mixer.addInput(envelope)
         }
         
-        engine.output = mixerWithEnvelope
+        engine.output = mixer
         
         do {
             try engine.start()
@@ -84,35 +78,57 @@ class ChordPlayer {
         }
     }
     
-    func playFor(duration: Double) {
-        print("Playing note")
-        for osc in oscillators {
-            osc.start()
+    func noteOn(notes: [Int], for duration: Double) {
+        print("Playing notes")
+        var i = 0
+        for note in notes {
+            let osc = oscillators[i]
+            let envelope = envelopes[i]
+            osc.frequency = AUValue(note).midiNoteToFrequency()
+            envelope.openGate()
+            i += 1
         }
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + duration, execute: {
-            for osc in self.oscillators {
-                osc.stop()
-            }
-        })
+    }
+    
+    func noteOff() {
+        for envelope in envelopes {
+            envelope.closeGate()
+        }
+    }
+    
+    func createEnvelope(osc: Oscillator) -> AmplitudeEnvelope {
+        var env = AmplitudeEnvelope(osc)
+        env.attackDuration = 0.01
+        env.decayDuration = 0.0
+        env.sustainLevel = 1.0
+        env.releaseDuration = 0.1
+        return env
     }
 }
 
 struct ChordButton: View {
     var title: String
-    var action: () -> Void
-
+    var onPress: () -> Void
+    var onUnpress: () -> Void
+    
     var body: some View {
-        Button(action: action) {
-            Text(title)
-                .frame(width: 200, height: 50) // Set the size of the button
-                .foregroundColor(.white)
-                .background(Color.blue)
-                .cornerRadius(10) // Rounded corners
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.black, lineWidth: 2) // Add a border
-                )
-        }
+        Text(title)
+            .frame(width: 200, height: 50) // Set the size of the button
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .cornerRadius(10) // Rounded corners
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.black, lineWidth: 2) // Add a border
+            )
+            .gesture(
+                DragGesture(minimumDistance: 0)
+                    .onChanged { _ in
+                        onPress()
+                    }
+                    .onEnded { _ in
+                        onUnpress()
+                    }
+            )
     }
 }
