eceedc046d3e6e4c893ee165782f2980e3f1589c
Sam Hodak <slhodak@gmail.com>
2023-12-21 19:45:24 -0300

    remove debug lines

--- iOS/SongHelper/Vision/Shapes.swift
+++ iOS/SongHelper/Vision/Shapes.swift
@@ -17,8 +17,6 @@ struct Dot: Shape {
         var path = Path()
         
         for point in points {
-//            print(point.x)
-//            print(point.y)
             path.addEllipse(in: CGRect(
                 x: point.x,
                 y: point.y,
@@ -27,15 +25,14 @@ struct Dot: Shape {
             )
         }
         
-//        return path
-
         return transformed(path: path)
     }
     
     private func transformed(path: Path) -> Path {
         return path
             .applying(
-                CGAffineTransform.identity.scaledBy(x: size.width, y: size.height))
+                CGAffineTransform.identity
+                    .scaledBy(x: size.width, y: size.height))
             .applying(
                 CGAffineTransform(scaleX: -1, y: -1)
                     .translatedBy(x: -size.width, y: -size.height))
